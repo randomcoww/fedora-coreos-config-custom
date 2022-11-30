a1b8ca2bd7e38d0ee994af1cf5a861923cd805dd
randomcoww <etherblimp@gmail.com>
2022-11-29 19:54:29 -0800

    remove unused

--- overlay.d/01custom/usr/lib/dracut/modules.d/35coreos-live/module-setup.sh
+++ overlay.d/01custom/usr/lib/dracut/modules.d/35coreos-live/module-setup.sh
@@ -32,24 +32,9 @@ install() {
     inst_simple "$moddir/live-generator" \
         "$systemdutildir/system-generators/live-generator"
 
-    # inst_simple "$moddir/coreos-live-unmount-tmpfs-var.sh" \
-    #     "/usr/sbin/coreos-live-unmount-tmpfs-var"
-
     inst_simple "$moddir/coreos-livepxe-rootfs.sh" \
         "/usr/sbin/coreos-livepxe-rootfs"
 
-    # install_and_enable_unit "coreos-live-unmount-tmpfs-var.service" \
-    #     "initrd-switch-root.target"
-
     install_and_enable_unit "coreos-livepxe-rootfs.service" \
         "initrd-root-fs.target"
-
-    # install_and_enable_unit "coreos-live-clear-sssd-cache.service" \
-    #     "ignition-complete.target"
-
-    # install_and_enable_unit "coreos-liveiso-persist-osmet.service" \
-    #     "default.target"
-
-    # install_and_enable_unit "coreos-livepxe-persist-osmet.service" \
-    #     "default.target"
 }
